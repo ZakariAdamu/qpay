3f64d1b312edd42c5f671aaa65c17f44f715c645
Zakari <zakari.adamu714@gmail.com>
2026-06-30 15:25:18 +0100

    refactor(git): optimize commit msg status print

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-	return <div className="flex-center text14_padding10">Home</div>;
+	return <div className="flex-center text14_padding10">Home - Landing Page</div>;
 };
 
 export default Home;
